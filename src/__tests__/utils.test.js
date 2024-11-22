301c91179ed1b59ffd88beb1976f4051cb6ad516
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-11-22 16:25:45 +0300

    First Trial

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,42 @@
-// Your tests here
+import isPalindrome from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true if a string is a palindrome", () => {
+        const aPalindrome = "racecar";
+
+        const answer = isPalindrome(aPalindrome);
+
+        expect(answer).toBe(true)
+    })
+    it("returns false if a string isn't a palindrome", () => {
+        const notAPalindrome = "car";
+
+        const answer = isPalindrome(notAPalindrome);
+
+        expect(answer).toBe(false);
+    })
+    it("detects palindromes regardless of casing", () => {
+        const mixedCasePalindrome = "RaceCar";
+
+        const answer = isPalindrome(mixedCasePalindrome);
+
+        expect(answer).toBe(true);
+    })
+    it("returns false for an empty string", () => {
+        const emptyString = ""
+
+        const answer = isPalindrome(emptyString);
+
+        expect(answer).toBe(false)
+    })
+    it("throws an error if input has non-alphabetic characters", () => {
+        expect(() => {
+            isPalindrome("Rac3Car");
+        }).toThrow(new Error("Non-alphabetic characters detected"));
+    })
+    it("throws an error if input isn't a string", () => {
+        expect(() => {
+            isPalindrome({});
+        }).toThrow(new Error("Input isn't a string"));
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
-// Your code here
+function isPalindrome(param) {
+
+    if (typeof(param) === "string" && param !== "") {
+
+        const reversedWord = param.split("").toReversed().join("");
+        
+        if (/^[A-Za-z]+$/.test(param) !== true) {
+            throw new Error("Non-alphabetic characters detected");
+        } else {
+            if (/^[A-Za-z]+$/.test(param) !== false && reversedWord.toLowerCase() === param.toLowerCase()) {
+                return true;
+            } else return false;
+        }
+    } else if (typeof(param) === "string" && param === "") {
+        return false;
+    } else throw new Error("Input isn't a string")
+
+}
+
+export default isPalindrome;
